9151e883188ed66a55920ac0cbed0e0cacdb2386
Etienne Bovet <ebovet@gmail.com>
2020-08-17 18:16:26 +1000

    Added contact us and about us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
+  get 'contact-us', to: 'pages#contact_us'
+  get 'about-us', to: 'pages#about_us'
 end
