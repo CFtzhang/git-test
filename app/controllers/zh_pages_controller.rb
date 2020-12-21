1c21e0143f429754d1e64df5b9771c369881432f
Etienne Bovet <ebovet@gmail.com>
2020-12-21 15:28:39 +1100

    Added zh pages for Chinese content

---
+++ app/controllers/zh_pages_controller.rb
@@ -0,0 +1,44 @@
+class ZhPagesController < ApplicationController
+  def index
+  end
+
+  def work_with_us
+  end
+
+  def reasearch_and_development
+  end
+
+  def about_us
+  end
+
+  def contact_us
+  end
+
+  def products
+  end
+
+  def careers
+  end
+
+  def generic_drug_products
+  end
+
+  def new_drug_products
+  end
+
+  def consumer_products
+    @consumer_products = Product.where(product_type: 'Consumer')
+  end
+
+  def therapeutic_areas
+  end
+
+  def dosage_forms
+  end
+
+  def advanced_facilities
+  end
+
+  def services
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,21 @@ Rails.application.routes.draw do
   get 'advanced-facilities', to: 'pages#advanced_facilities'
   get 'careers', to: 'pages#careers'
   get 'services', to: 'pages#services'
+
+  get 'zh/contact-us', to: 'zh_pages#contact_us'
+  get 'zh/', to: 'zh_pages#index'
+  get 'zh/about-us', to: 'zh_pages#about_us'
+  get 'zh/work-with-us', to: 'zh_pages#work_with_us'
+  get 'zh/research-and-development-platform', to: 'zh_pages#research_and_development_platform'
+  get 'zh/products', to: 'zh_pages#zh_products'
+  get 'zh/generic-drug-products', to: 'zh_pages#generic_drug_products'
+  get 'zh/new-drug-products', to: 'zh_pages#new_drug_products'
+  get 'zh/consumer-products', to: 'zh_pages#consumer_products'
+  get 'zh/therapeutic-areas', to: 'zh_pages#therapeutic_areas'
+  get 'zh/dosage-forms', to: 'zh_pages#dosage_forms'
+  get 'zh/advanced-facilities', to: 'zh_pages#advanced_facilities'
+  get 'zh/careers', to: 'zh_pages#careers'
+  get 'zh/services', to: 'zh_pages#services'
+
   resources :products, only: [:show]
 end
