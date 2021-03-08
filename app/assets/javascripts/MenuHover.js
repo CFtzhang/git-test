2d9e1354b8330ef10e80df7066f8b22b052d7b0c
Etienne Bovet <ebovet@gmail.com>
2021-03-08 13:24:47 +1100

    making navbar mobile friendly

--- app/assets/javascripts/MenuHover.js
+++ app/assets/javascripts/MenuHover.js
@@ -3,7 +3,7 @@ const MenuHover = function(e){
   const menu = document.getElementById('menuIcon')
   const mobileNavbarList = document.getElementById('mobile-navbar-list')
   if (typeof menu !== 'undefined' && menu !== null) {
-    menu.addEventListener("mousedown", () => mobileNavbarList.classList.toggle('mobile-navigationbar-extend'));
+    menu.addEventListener("mousedown", () => mobileNavbarList.classList.toggle('mobile-navigationbar-extend'))
   };
 
 };
